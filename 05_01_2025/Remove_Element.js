fa6973916a0b0da4990d1c26e12f5f69a062f4fc
Steven Lam <stevenl.inbox@gmail.com>
2025-05-01 18:16:29 -0700

    Finished remove element

---
+++ 05_01_2025/Remove_Element.js
@@ -0,0 +1,22 @@
+/*
+
+Same idea as remove duplicates from sorted array, but just check val and start at 0 instead
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  if (nums.length === 0) return 0;
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+};
