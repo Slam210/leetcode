1386bc0715fbb2d85808a489a4af86808b25a2f4
Steven Lam <stevenl.inbox@gmail.com>
2025-06-02 16:23:06 -0700

    Finished Permutations II using a modified backtracking

---
+++ 06_02_2025/PermutationsII.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function (nums) {
+  const result = [];
+  nums.sort((a, b) => a - b);
+  const used = Array(nums.length).fill(false);
+
+  function backtrack(path) {
+    if (path.length === nums.length) {
+      result.push([...path]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i]) continue;
+
+      if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+
+      used[i] = true;
+      path.push(nums[i]);
+      backtrack(path);
+      path.pop();
+      used[i] = false;
+    }
+  }
+
+  backtrack([]);
+  return result;
+};
+
+console.log(permuteUnique([1, 2, 1]));
