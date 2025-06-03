d4e85453391a36dc5a378b2b0a192c8584684cba
Steven Lam <stevenl.inbox@gmail.com>
2025-06-03 16:44:13 -0700

    Finished Rotate List with circular method

---
+++ 06_03_2025/RotateList.js
@@ -0,0 +1,28 @@
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function (head, k) {
+  if (!head || !head.next || k === 0) return head;
+
+  let length = 1;
+  let tail = head;
+  while (tail.next) {
+    tail = tail.next;
+    length++;
+  }
+
+  tail.next = head;
+
+  let stepsToNewTail = length - (k % length);
+  let newTail = head;
+  for (let i = 1; i < stepsToNewTail; i++) {
+    newTail = newTail.next;
+  }
+
+  let newHead = newTail.next;
+  newTail.next = null;
+
+  return newHead;
+};
