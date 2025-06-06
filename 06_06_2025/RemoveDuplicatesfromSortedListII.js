fb1a163c0edf3f420bdd69a18d43f3e028813202
Steven Lam <stevenl.inbox@gmail.com>
2025-06-06 15:06:30 -0700

    Added example and fixed typo in original answer

--- 06_06_2025/RemoveDuplicatesfromSortedListII.js
+++ 06_06_2025/RemoveDuplicatesfromSortedListII.js
@@ -27,9 +27,42 @@ var deleteDuplicates = function (head) {
       prev.next = current;
     } else {
       prev = current;
-      current = current.ext;
+      current = current.next;
     }
   }
 
   return dummy.next;
 };
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// Helper to convert array to ListNode
+function arrayToList(arr) {
+  const dummy = new ListNode(0);
+  let current = dummy;
+  for (let num of arr) {
+    current.next = new ListNode(num);
+    current = current.next;
+  }
+  return dummy.next;
+}
+
+// Helper to print list as array
+function printList(head) {
+  const result = [];
+  while (head) {
+    result.push(head.val);
+    head = head.next;
+  }
+  return result;
+}
+
+// Example
+const input = arrayToList([1, 2, 3, 3, 4, 4, 5]);
+const output = deleteDuplicates(input);
+console.log(printList(output)); // [1, 2, 5]
