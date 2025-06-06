e47df0b075f5266ccba2fea6308c948f8498b8c0
Steven Lam <stevenl.inbox@gmail.com>
2025-06-05 18:51:48 -0700

    Finished Search a 2d Matrix using a binary search

---
+++ 06_05_2025/Searcha2DMatrix.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+
+  let left = 0;
+  let right = m * n - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const row = Math.floor(mid / n);
+    const col = mid % n;
+    const midVal = matrix[row][col];
+
+    if (midVal === target) {
+      return true;
+    } else if (midVal < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+};
+
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    3
+  )
+);
