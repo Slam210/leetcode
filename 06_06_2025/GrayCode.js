4aadc6cb39a5114372840fc5c5d5c91f085fa247
Steven Lam <stevenl.inbox@gmail.com>
2025-06-06 18:03:39 -0700

    Finished Gray Code using bitwise xor

---
+++ 06_06_2025/GrayCode.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function (n) {
+  const result = [];
+  const total = 1 << n;
+
+  for (let i = 0; i < total; i++) {
+    result.push(i ^ (i >> 1));
+  }
+
+  return result;
+};
+
+console.log(grayCode(2));
