52c79cf5e4960cfd678744adf13ec84b8587f564
Steven Lam <stevenl.inbox@gmail.com>
2025-06-17 11:08:29 -0700

    Solved Number of 1 Bits using bit manipulation

--- 06_17_2025/Numberof1Bits.py
+++ 06_17_2025/Numberof1Bits.py
@@ -1,3 +1,11 @@
+"""
+
+The intuition behind this problem is to realize that bit manipulation
+will remove the least significant bit from the digit if we perform it between
+n and n-1. Thus, we can loop until we reach 0, which will indicate how many one's there
+are in n.
+
+"""
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
@@ -12,3 +20,10 @@ if __name__ == "__main__":
     
     for n in test_cases:
         print(f"Hamming weight of {n} (binary {bin(n)}): {solution.hammingWeight(n)}")
+        
+"""
+
+The run time is O(n) where n is the number of 1 bits
+Space complexity is O(1) since no other data structures are made
+
+"""
