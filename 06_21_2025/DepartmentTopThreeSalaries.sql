75f2940d42a595919f06025f4fcb30657c6f0c5c
Steven Lam <stevenl.inbox@gmail.com>
2025-06-21 15:52:56 -0700

    Finished Department Top Three Salaries using Counts

---
+++ 06_21_2025/DepartmentTopThreeSalaries.sql
@@ -0,0 +1,45 @@
+/*
+
+For each employee, the query shoud count how many distinct salaries in their department
+are greater than or equal to theirs. If this count is 3 or less, it means the
+employee’s salary is among the top three unique salaries in that department. 
+This way, the query identifies all employees who earn one of the top three 
+salaries.
+
+*/
+
+/*
+Example Table
+*/
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int)
+Create table If Not Exists Department (id int, name varchar(255))
+Truncate table Employee
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '85000', '1')
+insert into Employee (id, name, salary, departmentId) values ('2', 'Henry', '80000', '2')
+insert into Employee (id, name, salary, departmentId) values ('3', 'Sam', '60000', '2')
+insert into Employee (id, name, salary, departmentId) values ('4', 'Max', '90000', '1')
+insert into Employee (id, name, salary, departmentId) values ('5', 'Janet', '69000', '1')
+insert into Employee (id, name, salary, departmentId) values ('6', 'Randy', '85000', '1')
+insert into Employee (id, name, salary, departmentId) values ('7', 'Will', '70000', '1')
+Truncate table Department
+insert into Department (id, name) values ('1', 'IT')
+insert into Department (id, name) values ('2', 'Sales')
+
+/*
+Solution
+*/
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM
+    Employee e
+    JOIN Department d ON e.departmentId = d.id
+WHERE
+    (
+        SELECT COUNT(DISTINCT salary)
+        FROM Employee e2
+        WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
+    ) <= 3
+ORDER BY
+    Department, Salary DESC;
