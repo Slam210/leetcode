7ee079078a4fc11c2d4aa643053e114d302bd7f3
Steven Lam <stevenl.inbox@gmail.com>
2025-09-05 09:55:45 -0700

    Solved Queries Quality and Percentage using ROUND, AVERAGE, SUM, COUNT

---
+++ 09_2025/09_05_2025/QueriesQualityandPercentage.sql
@@ -0,0 +1,17 @@
+-- Example Table
+Create table If Not Exists Queries (query_name varchar(30), result varchar(50), position int, rating int)
+Truncate table Queries
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Golden Retriever', '1', '5')
+insert into Queries (query_name, result, position, rating) values ('Dog', 'German Shepherd', '2', '5')
+insert into Queries (query_name, result, position, rating) values ('Dog', 'Mule', '200', '1')
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Shirazi', '5', '2')
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Siamese', '3', '3')
+insert into Queries (query_name, result, position, rating) values ('Cat', 'Sphynx', '7', '4')
+
+-- Solution
+SELECT 
+    query_name, 
+    round (avg(rating/position),2) as quality,
+    round(sum(case when rating < 3 then 1 else 0 end)*100/count(*),2) as poor_query_percentage
+FROM queries
+GROUP BY  query_name
